773c84c1c1504df06cbc977eac30a23b5157e87c
Sun Phil Kwon <fsw0422@gmail.com>
2025-11-21 16:00:51 +0100

    Updated message when testing, to create a fresh devcontainer (not image)

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "If you want to test everything from fresh, please remove the current running devcontainer image and recreate it."
+echo "If you want to test the installation fresh, please create a fresh devcontainer."
 read -p "Type 'yes' to proceed to install: " response
 if [ "$response" != "yes" ]; then
 	echo "Installation aborted."
